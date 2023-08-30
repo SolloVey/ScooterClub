4dbf9651d97404e203ceba108443299784b216bf
SolloVey <chelnokow@gmail.com>
2023-08-30 10:05:08 +0300

    Desctop version ready

--- script/main.js
+++ script/main.js
@@ -1,1 +1,26 @@
-console.log('Started!!!');
+// const wheelInner = document.querySelectorAll('.wheel__inner');
+// const wheelContent = document.querySelectorAll('.wheel__content');
+
+// console.log(wheelInner);
+
+// function rotateText(value) {
+// 	let deg = -34;
+// 	for (let item of wheelInner) {
+// 		item.style.transform = `rotate(${deg}deg)`;
+// 		deg += value;
+// 	}
+// 	return value;
+// }
+
+// function letterSpacing(value) {
+// 	{
+// 		let deg = -34;
+// 		for (let item of wheelContent) {
+// 			item.style.transform = `rotate(${deg}deg)`;
+// 			deg += value;
+// 		}
+// 	}
+// }
+
+// rotateText(30);
+// letterSpacing(35);
