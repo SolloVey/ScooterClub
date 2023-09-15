4c90a904556669c0628775788a31e5f0fb9e0a87
SolloVey <chelnokow@gmail.com>
2023-09-15 17:36:34 +0300

    Add adaptive

--- script/main.js
+++ script/main.js
@@ -1,3 +1,24 @@
+const swiper = new Swiper('.swiper', {
+	// Optional parameters
+	// loop: true,
+
+	// If we need pagination
+	// pagination: {
+	// 	el: '.swiper-pagination',
+	// },
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+
+	// And if we need scrollbar
+	// scrollbar: {
+	// 	el: '.swiper-scrollbar',
+	// },
+});
+
 // const wheelInner = document.querySelectorAll('.wheel__inner');
 // const wheelContent = document.querySelectorAll('.wheel__content');
 
