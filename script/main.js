0fde055aa13c9671db7cc022bef3cad500a09901
SolloVey <chelnokow@gmail.com>
2023-09-15 17:40:10 +0300

    Add slader script

--- script/main.js
+++ script/main.js
@@ -1,22 +1,8 @@
 const swiper = new Swiper('.swiper', {
-	// Optional parameters
-	// loop: true,
-
-	// If we need pagination
-	// pagination: {
-	// 	el: '.swiper-pagination',
-	// },
-
-	// Navigation arrows
 	navigation: {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
 	},
-
-	// And if we need scrollbar
-	// scrollbar: {
-	// 	el: '.swiper-scrollbar',
-	// },
 });
 
 // const wheelInner = document.querySelectorAll('.wheel__inner');
