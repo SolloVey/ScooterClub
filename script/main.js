f0df4cb28f71df3f842b80be99b4abad76769d88
SolloVey <chelnokow@gmail.com>
2023-09-16 13:37:50 +0300

    Edits

--- script/main.js
+++ script/main.js
@@ -4,30 +4,3 @@ const swiper = new Swiper('.swiper', {
 		prevEl: '.swiper-button-prev',
 	},
 });
-
-// const wheelInner = document.querySelectorAll('.wheel__inner');
-// const wheelContent = document.querySelectorAll('.wheel__content');
-
-// console.log(wheelInner);
-
-// function rotateText(value) {
-// 	let deg = -34;
-// 	for (let item of wheelInner) {
-// 		item.style.transform = `rotate(${deg}deg)`;
-// 		deg += value;
-// 	}
-// 	return value;
-// }
-
-// function letterSpacing(value) {
-// 	{
-// 		let deg = -34;
-// 		for (let item of wheelContent) {
-// 			item.style.transform = `rotate(${deg}deg)`;
-// 			deg += value;
-// 		}
-// 	}
-// }
-
-// rotateText(30);
-// letterSpacing(35);
